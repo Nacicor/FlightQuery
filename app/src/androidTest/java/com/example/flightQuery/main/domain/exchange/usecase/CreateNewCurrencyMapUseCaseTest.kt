123ea69fba3739fda125ea047f4dcdec11f4949b
N acicor <sa456x12@gmail.com>
2024-06-15 13:51:09 +0800

    Create test

---
+++ app/src/androidTest/java/com/example/flightQuery/main/domain/exchange/usecase/CreateNewCurrencyMapUseCaseTest.kt
@@ -0,0 +1,33 @@
+package com.example.flightQuery.main.domain.exchange.usecase
+
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+class CreateNewCurrencyMapUseCaseTest {
+    @Test
+    fun testUpdateCurrencyAmounts() {
+        runTest {
+            val newCurrencyMap = CreateNewCurrencyMapUseCase()
+
+            val fakeDisplay = mapOf("USD" to 1.0, "AUD" to 1.5)
+            val fakeRateList = mapOf("USD" to 1.0, "AUD" to 0.75)
+
+            val selectedCurrency = "USD"
+            val amount = 100.0
+
+            val fakeResult = mutableMapOf(
+                "USD" to amount,
+                "AUD" to BigDecimal(amount * fakeRateList["AUD"]!! / fakeRateList["USD"]!!).setScale(
+                    2, RoundingMode.HALF_UP
+                ).toDouble()
+            )
+            val actualResult =
+                newCurrencyMap.create(fakeDisplay, fakeRateList, selectedCurrency, amount)
+
+            assertEquals(fakeResult, actualResult)
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/example/flightQuery/main/domain/exchange/usecase/GetRateResponseUseCaseTest.kt
@@ -0,0 +1,5 @@
+package com.example.flightQuery.main.domain.exchange.usecase
+
+import org.junit.Assert.*
+
+class GetRateResponseUseCaseTest
