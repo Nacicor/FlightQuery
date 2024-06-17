d72a4426dca89f19efb36023920dd2e3bd5a00c0
N acicor <sa456x12@gmail.com>
2024-06-17 12:41:59 +0800

    Change RateRepository to interface

--- app/src/main/java/com/example/flightQuery/main/data/exchange/RateRepository.kt
+++ app/src/main/java/com/example/flightQuery/main/data/exchange/RateRepository.kt
@@ -3,8 +3,6 @@ package com.example.flightQuery.main.data.exchange
 import com.example.flightQuery.main.domain.exchange.model.RateInfoItem
 import retrofit2.Response
 
-class RateRepository(private val apiService: RateApiService) {
-    suspend fun getFromApiResult(): Response<RateInfoItem> {
-        return apiService.getRate("fca_live_TQcP4fiHbbuay9cZNFEFJ9v7mO7QYPyqTdkIZiRX")
-    }
+interface RateRepository {
+    suspend fun getFromApiResult(): Response<RateInfoItem>
 }
